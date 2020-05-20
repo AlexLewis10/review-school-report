f60fefbb0683fa35d0817430c68199fb667f02c1
Alex Lewis <alexlewis374@gmail.com>
2020-05-20 12:17:22 +0100

    test split string into an array

--- spec/ReportSpec.js
+++ spec/ReportSpec.js
@@ -13,5 +13,15 @@ describe('Report', function () {
         it('when given amber it returns the colour amber', function () {
             expect(report.returnColour("Amber")).toEqual("Amber")
         })
+
+        it('when given red it returns the colour red', function () {
+            expect(report.returnColour("Red")).toEqual("Red")
+        })
+    })
+
+    describe('Splits the string into an array', function () {
+        it('when given a string with 2 or more words it separates to an array', function () {
+            expect(report.splitString("Green, Green")).toEqual(["Green", "Green"])
+        })
     })
 })
--- src/Report.js
+++ src/Report.js
@@ -5,6 +5,18 @@ function Report () {
 Report.prototype.returnColour = function (gradeString) {
     if (gradeString === "Green") {
         return "Green"
+    } else if (gradeString === "Amber") {
+        return "Amber"
     }
-    return "Amber"
+    return "Red"
 }
+
+Report.prototype.splitString = function (gradeString) {
+    var gradeArray = gradeString.split(', ')
+    console.log(gradeArray)
+    return gradeArray
+}
+
+
+// var array = gradeString.split(', ')
+// console.log(array)
